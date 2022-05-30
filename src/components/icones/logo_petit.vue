1df6964d28eb04ad37144e4c9dc370a5dd9ea9c4
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-05-30 09:13:04 +0200

    Ajout logo (header)

---
+++ src/components/icones/logo_petit.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="32.5" cy="32.5" r="32" fill="#BA58CE" stroke="#E0D400"/>
+<rect x="12.5806" y="23.0646" width="2.09677" height="18.871" rx="1.04839" fill="#E0D400"/>
+<rect x="9.43542" y="27.2581" width="2.09677" height="11.5323" rx="1.04839" fill="#E0D400"/>
+<rect x="47.1774" y="23.0646" width="2.09677" height="18.871" rx="1.04839" fill="#E0D400"/>
+<rect x="25.1613" y="25.1611" width="2.09677" height="13.629" rx="1.04839" fill="#E0D400"/>
+<rect x="28.3064" y="25.1611" width="2.09677" height="13.629" rx="1.04839" fill="#E0D400"/>
+<rect x="31.4515" y="25.1611" width="2.09677" height="13.629" rx="1.04839" fill="#E0D400"/>
+<rect x="34.5967" y="27.2581" width="2.09677" height="9.43548" rx="1.04839" fill="#E0D400"/>
+<rect x="37.742" y="27.2581" width="2.09677" height="9.43548" rx="1.04839" fill="#E0D400"/>
+<rect x="40.8871" y="27.2581" width="2.09677" height="9.43548" rx="1.04839" fill="#E0D400"/>
+<rect x="50.3226" y="27.2581" width="2.09677" height="9.43548" rx="1.04839" fill="#E0D400"/>
+<rect x="15.7258" y="19.9194" width="2.09677" height="25.1613" rx="1.04839" fill="#E0D400"/>
+<rect x="44.0323" y="19.9194" width="2.09677" height="25.1613" rx="1.04839" fill="#E0D400"/>
+<rect x="22.0161" y="19.9194" width="2.09677" height="25.1613" rx="1.04839" fill="#E0D400"/>
+<rect x="18.871" y="14.6775" width="2.09677" height="33.5484" rx="1.04839" fill="#E0D400"/>
+</svg>
+
+</template>
