39d31090a531c6f36422fa6c0cfc23ac997f9a1e
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-05-30 09:47:10 +0200

    Header & bouton terminé (pas responsive)

---
+++ src/components/bouton.vue
@@ -0,0 +1,26 @@
+<template>
+<a :href="url">
+    <button class="bg-jaune2 shadow-2xl shadow-Jaune rounded-10xl p-3 hover:bg-violet2 group">
+    <p class="text-center text-black group-hover:text-white">
+    {{txt}}
+      </p>
+</button>
+</a>
+</template>
+
+<script>
+
+export default {
+props: {
+    txt: {
+      type: String,
+      default: "",
+    },
+
+    url: {
+        type: String,
+        default: "#",
+    }
+}
+};
+</script>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,29 @@
 <template>
-<h1>header</h1>
+
+<div class="w-full bg-violet2 border-b-8 border-jaune2">
+    <div class="flex">
+        <RouterLink to="/"> <logo class="scale-75"/> </RouterLink>
+        <div class="flex ml-auto mr-auto gap-16 my-6">
+        <RouterLink to="/programmation"> <p class="text-3xl text-center text-jaune2 hover:text-white font-mono">Programme</p> </RouterLink>
+        <p class="text-white text-3xl"> | </p>
+        <RouterLink to="/lesartistes"> <p class="text-3xl text-center text-jaune2 hover:text-white font-mono">Artistes</p> </RouterLink>
+        </div>
+        <bouton class="ml-auto my-6"
+        txt="Commander un billet"
+        />
+    </div>
+</div>
+
 </template>
 
 <script>
+import logo from "./icones/logo_grand.vue"
+import bouton from "./bouton.vue"
+
 import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  components:{ logo, bouton }
+};
+
 </script>
