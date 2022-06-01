32cd4fd643aa746be42b86135503254318616d05
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-06-01 21:03:46 +0200

    Ajout profil artiste design

--- src/Views/artisteView.vue
+++ src/Views/artisteView.vue
@@ -1,7 +1,49 @@
 <template>
 
-    <h1>L'artiste X</h1>
+<div class="grid lg:grid-cols-2 mb-44 mt-20 gap-x-10">
+    <profil 
+    nom="Angèle"
+    genre="Pop Française"
+    :portrait="angele"
+    
+    instagram="#"
+    facebook="#"
+    youtube="#"/>
+    <div>
+        <h1>Angèle</h1>
+        <p>Angèle Van Laeken naît le 3 décembre 1995 à Uccle en Belgique, commune bourgeoise du sud de la région de Bruxelles-Capitale. Elle est la fille du chanteur Marka et de la comédienne Laurence Bibot (fondateurs du duo pop-rock-électro Monsieur et Madame), et la sœur du rappeur Roméo Elvis... <br>
+            <a href="#"> <span class="text-Violet text-sm"> Voir Plus... </span> </a>
+        </p>
+    </div>
+
+</div>
+
 </template>
 
 <script>
+import profil from "../components/profilartiste.vue"
+
+import angele from "../assets/img/chanteurs/angele.jpg"
+import amir from "../assets/img/chanteurs/amir.jpg"
+import lisa from "../assets/img/chanteurs/lisa.jpg"
+import akb48 from "../assets/img/chanteurs/AKB48.jpg"
+import bts from "../assets/img/chanteurs/BTS.jpg"
+import loona from "../assets/img/chanteurs/Loona.jpg"
+
+export default {
+
+      data: function() {
+    return {
+    angele,
+    amir,
+    lisa,
+    akb48,
+    bts,
+    loona,
+    }
+  },
+
+    components:{ profil }
+};
+
 </script>
---
+++ src/components/profilartiste.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="grid grid-cols-2 w-1/2">
+        <div class="grid -mr-96">
+            <a :href="instagram"> <img src="./icones/instagram.png" alt="" class="w-1/12"></a>
+            <a :href="facebook"> <img src="./icones/facebook.png" alt="" class="w-1/12"></a>
+            <a :href="youtube"> <img src="./icones/youtube.png" alt="" class="w-1/12"></a> 
+        </div>
+
+        <div>
+            <img :src="portrait" alt="" class="border-r-8 border border-b-8 border-jaune2 ">
+            <div class="flex justify-center">
+                <h1 class="font-mono font-semibold">{{ nom }}</h1>
+            </div>
+            <div class="flex justify-center">
+                <micro class="w-1/5"/>
+                <p class="text-center font-serif">{{ genre }}</p>
+            </div>
+        </div>
+</div>
+</template>
+
+<script>
+import micro from "./icones/micro.vue"
+
+export default {
+    components:{ micro },
+
+    props:{
+        nom:{
+            type: String,
+        },
+
+        portrait:{
+            type: String,
+        },
+
+        genre:{
+            type: String,
+        },
+
+        instagram:{
+            type: String,
+        },
+
+        facebook:{
+            type: String,
+        },
+
+        youtube:{
+            type: String,
+        },
+    }
+};
+
+</script>
